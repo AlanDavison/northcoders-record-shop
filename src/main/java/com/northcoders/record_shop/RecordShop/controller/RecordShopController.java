2674a14ee414c2849feb333216d79090254f78a1
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-05 15:40:15 +0100

    Add addAlbum endpoint.

--- src/main/java/com/northcoders/record_shop/RecordShop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/record_shop/RecordShop/controller/RecordShopController.java
@@ -5,10 +5,7 @@ import com.northcoders.record_shop.RecordShop.service.RecordShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,11 @@ public class RecordShopController {
     @GetMapping("/all")
     public ResponseEntity<List<Album>> getAllRecords() {
         return new ResponseEntity<>(this.service.getAllAlbums(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Album> addAlbum(@RequestBody Album album) {
+        return new ResponseEntity<>(this.service.addAlbum(album), HttpStatus.CREATED);
     }
 
     @GetMapping("/{recordId}")

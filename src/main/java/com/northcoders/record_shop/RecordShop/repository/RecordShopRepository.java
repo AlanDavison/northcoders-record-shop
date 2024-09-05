c37621b11cb7fed3a649567d4d60955cd1d8eed8
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-05 10:48:25 +0100

    Remove unnecessary import.

--- src/main/java/com/northcoders/record_shop/RecordShop/repository/RecordShopRepository.java
+++ src/main/java/com/northcoders/record_shop/RecordShop/repository/RecordShopRepository.java
@@ -3,7 +3,5 @@ package com.northcoders.record_shop.RecordShop.repository;
 import com.northcoders.record_shop.RecordShop.model.Album;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface RecordShopRepository extends CrudRepository<Album, Long> {
 }

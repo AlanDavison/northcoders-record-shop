b4869a95fefe374971a0ce6199072a7e1dd11cbb
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-05 10:02:00 +0100

    Initial commit.

---
+++ src/test/java/com/northcoders/record_shop/RecordShop/RecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.record_shop.RecordShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

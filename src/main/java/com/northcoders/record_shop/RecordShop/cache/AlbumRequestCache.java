79dbbb0e27585445e570755b52230696cfaa3203
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-10 16:55:31 +0100

    Caching!

--- src/main/java/com/northcoders/record_shop/RecordShop/cache/AlbumRequestCache.java
+++ src/main/java/com/northcoders/record_shop/RecordShop/cache/AlbumRequestCache.java
@@ -12,19 +12,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.time.Instant;
+import java.util.*;
 
 @Component
 public class AlbumRequestCache {
     Logger logger = LoggerFactory.getLogger(AlbumRequestCache.class);
-
-    @Autowired
     private RecordShopRepository repo;
 
     private HashMap<Long, CachedAlbum> requestCache = new HashMap<>();
+
+    @Autowired
+    public AlbumRequestCache(RecordShopRepository repo) {
+        this.repo = repo;
+
+        ArrayList<Album> firstTenAlbums = new ArrayList<>();
+
+        for (Album a: this.repo.findAll()) {
+            if (firstTenAlbums.size() >= 10)
+                break;
+
+            firstTenAlbums.add(a);
+        }
+
+        for (Album a: firstTenAlbums) {
+            this.requestCache.put(a.getId(), new CachedAlbum(a));
+        }
+    }
+
+    public Optional<List<Album>> getAllAlbums() {
+        ArrayList<Album> albums = new ArrayList<>();
+        Optional<List<Album>> optionalAlbums;
+
+        for (Long id: this.requestCache.keySet()) {
+            Optional<Album> album = this.getAlbumById(id);
+
+            if (album.isPresent())
+                albums.add(album.get());
+        }
+
+        optionalAlbums = Optional.of(albums);
+
+        if (optionalAlbums.get().isEmpty())
+            return Optional.empty();
+
+        return optionalAlbums;
+    }
 
     public Optional<Album> getAlbumById(Long id) {
         if (this.requestCache.containsKey(id)) {

b0bc40eaa795a224d8c730e2e6026061ceaf6492
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-11 09:46:11 +0100

    Add Artist crud repository.

---
+++ src/main/java/com/northcoders/record_shop/RecordShop/repository/ArtistRepository.java
@@ -0,0 +1,7 @@
+package com.northcoders.record_shop.RecordShop.repository;
+
+import com.northcoders.record_shop.RecordShop.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArtistRepository extends CrudRepository<Artist, Long> {
+}

14d8660c9d9182539935125b9b47981b776da5ab
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-11 09:45:05 +0100

    Update service to allow for artist functionality.

--- src/main/java/com/northcoders/record_shop/RecordShop/service/RecordShopService.java
+++ src/main/java/com/northcoders/record_shop/RecordShop/service/RecordShopService.java
@@ -1,6 +1,7 @@
 package com.northcoders.record_shop.RecordShop.service;
 
 import com.northcoders.record_shop.RecordShop.model.Album;
+import com.northcoders.record_shop.RecordShop.model.Artist;
 
 import java.util.List;
 
@@ -8,4 +9,8 @@ public interface RecordShopService {
     List<Album> getAllAlbums();
     Album addAlbum(Album album);
     Album getAlbumById(Long id);
+    Artist updateAlbum(Long id, Album album);
+    Artist addArtist(Artist artist);
+    Artist getArtistById(Long id);
+    Artist updateArtistById(Long id, Artist artist);
 }

d3f7750982b2b50bf2c5bbe7a7efe69b4bfa9de6
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-11 10:20:20 +0100

    Update exception handler for new exceptions.

---
+++ src/main/java/com/northcoders/record_shop/RecordShop/exception/IncorrectHttpRequestType.java
@@ -0,0 +1,7 @@
+package com.northcoders.record_shop.RecordShop.exception;
+
+public class IncorrectHttpRequestType extends RuntimeException {
+    public IncorrectHttpRequestType(String message) {
+        super(message);
+    }
+}

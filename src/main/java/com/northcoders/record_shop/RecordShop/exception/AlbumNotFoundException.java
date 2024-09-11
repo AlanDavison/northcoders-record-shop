628406e05ed2d5eb6776dc4b83b24491bf10fa21
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-11 10:20:40 +0100

    Refactor AlbumNotFoundException.

--- src/main/java/com/northcoders/record_shop/RecordShop/exception/AlbumNotFoundException.java
+++ src/main/java/com/northcoders/record_shop/RecordShop/exception/AlbumNotFoundException.java
@@ -1,13 +1,7 @@
 package com.northcoders.record_shop.RecordShop.exception;
 
 public class AlbumNotFoundException extends RuntimeException {
-    private String message;
-
     public AlbumNotFoundException(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return this.message;
+        super(message);
     }
 }
